ddbf6d08f9a32b22e775f466fc28591489060d37
Andrew Serianni <andrewserianni@Andrews-Mac-Pro.local>
2017-05-09 23:22:40 -0400

    Begin implementation of JWT authentication/authorization

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import SchemaDefinition from './schema';
 import Types from './Types';
 import resolvers from './resolvers';
+import jwt from 'express-jwt';
 import logger from '../utils/logger';
 import cors from 'cors';
 const config = require('config');
@@ -20,6 +21,14 @@ const schema = makeExecutableSchema({
   typeDefs: [SchemaDefinition, Types],
   resolvers
 });
+
+app.use('/',
+  jwt({secret: 'test'}),
+  (req, res) => {
+    if (!req.user.admin) return res.sendStatus(401);
+    res.sendStatus(200);
+  }
+);
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
